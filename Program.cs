f7cb490528a55558c9a40fee57b6314f9b7e771b
Jinal-Vaghela <vaghelajinal1@gamil.com>
2023-05-12 20:34:20 +0530

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+
+namespace jinal
+{
+    class jill
+    {
+        static void Main(string[] args)
+        {
+            string guess = "";
+            string sword = "swapnil";
+            int guessnumber = 0;
+            int guesslimit = 5;
+            bool gusscount = false;
+            
+            while(guess!=sword && !gusscount)
+            { if (guessnumber < guesslimit )
+            {
+                Console.WriteLine("Enter the squad member");
+                guess =Console.ReadLine();
+                guessnumber++;
+
+            }
+            else
+                {
+                    gusscount = true;
+                }
+            }
+            if (gusscount)
+            {
+                Console.WriteLine("You fucked upp!");
+            }
+            else
+            {
+                Console.WriteLine("You win!");
+            }
+        }
+
+    }
+}
